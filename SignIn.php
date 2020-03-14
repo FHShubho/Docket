bb89ce4cb373e7b2bb077f8ced6d34e840bfe4f6
sana-sanzida <61015366+sana-sanzida@users.noreply.github.com>
2020-03-14 23:18:33 +0600

    Added function to check email verification

--- SignIn.php
+++ SignIn.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost","root","","docket");
+$con=mysqli_connect("localhost","digibd_docket","docket","digibd_docket");
+//$con=mysqli_connect("localhost","root","","docket");
 if($con===false)
 {
  echo '<script type= "text/javascript"> alert("Database could not connect")</script>';
@@ -7,10 +8,13 @@ if($con===false)
 
 ?>
 <!DOCTYPE html>
+<html lang="en" class="no-js">
 
 <head>
-	<title>Welcome to Docket</title>
-    <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Dosis:400,700,500|Nunito:300,400,600' />
+  <title>Welcome to Docket</title>
+  <link rel='icon' href='favicon.png' type='image/x-icon'/>
+  <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Dosis:400,700,500|Nunito:300,400,600' />
+  <meta name=viewport content="width=device-width, initial-scale=0">
 	<link rel="stylesheet" href="css/plugins.css">
 	<link rel="stylesheet" href="css/style_sana.css">
 </head>
@@ -18,7 +22,7 @@ if($con===false)
 <body>
 <div class="loginContainer">
 	<div class="headerImage">
-		<a href="SignIn.html"><img class="logo" src="images/poster.png" alt="" ></a>
+		<a href="SignIn.php"><img class="logo" src="images/poster.png" alt="" ></a>
 <div class="form1">
     <div class="login-content">
         <h3>Login</h3>
@@ -76,15 +80,24 @@ if(isset($_POST['button']))
   if (mysqli_num_rows ($query_run) > 0)
   {
 
-    //session_start();
-    //$_SESSION['email'] = $email;
+    session_start();
+    $_SESSION['email'] = $email;
 
-    
-    echo "<script> location.href='UserProfile.html'; </script>";
-  }
-    else {
-      echo '<script type="text/javascript"> alert ("Invalid Login Info")</script>';
+    $sql ="SELECT * FROM UserInfo WHERE verified = 'yes' AND email='$email' ";
+    $query_run = mysqli_query($con,$sql);
+
+    if (mysqli_num_rows($query_run) > 0)
+    {
+      echo "<script> location.href='UserProfile.html'; </script>";
     }
+    else
+    {
+      echo '<script type="text/javascript"> alert ("Please Verify your email")</script>';
+    }
+  }
+  else {
+    echo '<script type="text/javascript"> alert ("Invalid Login Info")</script>';
+  }
 
 }
 
